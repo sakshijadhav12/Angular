3f14617bdf33ba41dd4395ec98ec1dd466e999e5
sakshijadhav12 <sakshijjadhav28@gmail.com>
2023-11-09 16:40:37 +0530

    created a compoent for registration

---
+++ src/app/Component/Registration.js
@@ -0,0 +1,24 @@
+<HTML>
+    <head>
+    <meta charset ="Utf8"></meta>
+    <meta name="viewport " content ="width=device-width"></meta>
+    <title>Registration</title>
+    </head>
+    <body>
+    <div class="container">
+        <h2>Create a Google Account</h2>
+        <form>
+            
+            <input type="text" id="" name="username" required>
+
+           
+            <input type="email" id="email" name="email" required>
+
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+
+            <button type="submit">Create Account</button>
+        </form>
+    </div>
+    </body>
+</HTML>
